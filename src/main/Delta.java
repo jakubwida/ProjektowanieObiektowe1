08aebed616bff08443e77fbab90d25473e7abfd4
kacper <kacper@crustlab.com>
2018-10-08 13:07:46 +0200

    Add Delta class

---
+++ src/main/Delta.java
@@ -0,0 +1,26 @@
+package main;
+
+public class Delta {
+
+    public void delta(double a, double b, double c) {
+        double delta;
+        if(a == 0){
+            System.out.println("Not a quadratic equation!");
+        } else {
+            delta = b * b - 4 * a * c;
+
+            if (delta < 0) {
+                System.out.println("No results");
+            }
+            if(delta == 0) {
+                System.out.printf("The equation has one element: %f", (-b / (2 * a)));
+            }
+            if(delta > 0) {
+                System.out.println("The equation has two elements:");
+            }
+                System.out.println("x1 = " + (-b - Math.sqrt(delta)) / (2 * a));
+                System.out.println("x2 = " + (-b + Math.sqrt(delta)) / (2 * a));
+            }
+    }
+
+}
